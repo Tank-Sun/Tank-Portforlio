586d926a0f600fea7285a9aa6032f9ba1262f471
Tank-Sun <thomastank0926@gmail.com>
2023-05-30 21:06:44 -0600

    change sendgrid layout

--- src/pages/api/sendgrid.js
+++ src/pages/api/sendgrid.js
@@ -10,7 +10,7 @@ async function sendEmail(req, res) {
     await sendgrid.send({
       to: "thomastank0926@gmail.com", // Your email where you'll receive emails
       from: "tanksunportfolio@gmail.com", // your website email address here
-      subject: `[Lead from website] : ${req.body.subject}`,
+      subject: `[Lead from portfolio website] : ${req.body.subject}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="en">
         <head>
@@ -23,7 +23,7 @@ async function sendEmail(req, res) {
 
         <body>
           <div class="container" style="margin-left: 20px;margin-right: 20px;">
-            <h3>You've got a new mail from ${req.body.name}, their email is: ✉️${req.body.email}, their phone number is ${req.body.phoneNumber} </h3>
+            <h3>You've got a new mail from ${req.body.name}, their email is: ✉️ ${req.body.email}, their phone number is ${req.body.phoneNumber}.</h3>
             <div style="font-size: 16px;">
               <p>Message:</p>
               <p>${req.body.message}</p>
